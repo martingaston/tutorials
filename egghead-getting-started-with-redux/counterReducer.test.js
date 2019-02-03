fc9f99c81269a3ddbac23bf5579e2e11ae8787c8
martingaston <squidman@gmail.com>
2019-02-03 11:21:28 +0000

    add counter reducer with tests (lesson 5)

---
+++ egghead-getting-started-with-redux/counterReducer.test.js
@@ -0,0 +1,16 @@
+const counter = require("./counterReducer");
+
+test("returns correct increment/decrement", () => {
+  expect(counter(0, { type: "INCREMENT" })).toBe(1);
+  expect(counter(1, { type: "INCREMENT" })).toBe(2);
+  expect(counter(2, { type: "DECREMENT" })).toBe(1);
+  expect(counter(1, { type: "DECREMENT" })).toBe(0);
+});
+
+test("returns current state if action is not understood", () => {
+  expect(counter(1, { type: "SQUARE" })).toBe(1);
+});
+
+test("returns initial state of the application if state argument is undefined", () => {
+  expect(counter(undefined, {})).toBe(0);
+});
