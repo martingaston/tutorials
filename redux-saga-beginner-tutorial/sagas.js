774b19c3c492d5ca571f6d9038ed2a676f0376a9
Martin Gaston <martin.gaston@redsift.io>
2019-03-12 17:32:53 +0000

    add comments to sagas

--- redux-saga-beginner-tutorial/sagas.js
+++ redux-saga-beginner-tutorial/sagas.js
@@ -7,20 +7,29 @@ export function* helloSaga() {
   console.log("Hello Sagas!")
 }
 
+// what is the need for the worker/watcher saga pattern?
+
 // worker saga 
 
 export function* incrementAsync() {
+  // when a promise is yielded, sagas will suspend until resolved
   yield delay(1000)
+  // when the promise completes it continues until the next yield
+  // put() is a saga Effect
+  // when the saga middleware recieves a saga Effect, it will also pause
   yield put({ type: 'INCREMENT' })
 }
 
 // watcher saga 
 
 export function* watchIncrementAsync() {
+  // takeEvery listens for dispatched actions 
   yield takeEvery('INCREMENT_ASYNC', incrementAsync)
 }
 
+
 export default function* rootSaga() {
+  // all instructs the saga middleware to run all functions in parallel
   yield all([
     helloSaga(),
     watchIncrementAsync(),
