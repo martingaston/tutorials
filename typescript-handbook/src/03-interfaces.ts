6d452d917b0c807bad58c8414e348b5f5b24a4f6
martingaston <squidman@gmail.com>
2019-03-17 22:09:02 +0000

    start interfaces

---
+++ typescript-handbook/src/03-interfaces.ts
@@ -0,0 +1,16 @@
+function printLabel(labelledObj: { label: string }) {
+  console.log(labelledObj.label);
+}
+
+let myObj = { size: 10, label: "Size 10 Object" };
+printLabel(myObj);
+
+interface LabelledValue {
+  label: string;
+}
+
+function printLabel2(labelledObj: LabelledValue) {
+  console.log(labelledObj.label);
+}
+
+printLabel2(myObj);
